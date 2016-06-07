9aac42fb737ab1ebcffc13cda968a26d6bd93352
kaiserj <kaiserj@uni-mainz.de>
2016-06-07 14:41:38 +0200

    Add small script to install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+go get github.com/cihub/seelog
+#go get code.google.com/p/goprotobuf/proto
+#go get code.google.com/p/goprotobuf/protoc-gen-go
+
+go get github.com/gogo/protobuf/proto
+go get github.com/gogo/protobuf/protoc-gen-gogo
+go get github.com/gogo/protobuf/gogoproto
